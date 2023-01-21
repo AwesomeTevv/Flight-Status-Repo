4116bc047e910ffaadaaf2c3b8681b5bc2ab48ce
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-01-21 12:15:06 +0200

    Starting

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get('https://www.flysafair.co.za/travel-tools/flight-status').text
+
+soup = BeautifulSoup(html_text, 'lxml')
+
+flights = soup.find_all('tbody')
