00301433a4c6c9885330d5fa5db9db8bbbc8842c
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-01-21 12:31:21 +0200

    Debugging

--- main.py
+++ main.py
@@ -8,19 +8,4 @@ soup = BeautifulSoup(html_text, 'lxml')
 flights = soup.find_all('tr')
 
 for flight in flights:
-    flight_details = flight.find_all('td')
-    
-    flight_num = flight_details[0].text
-    origin = flight_details[1].text
-    dest = flight_details[2].text
-    depart_time = flight_details[3].text
-    arrival_time = flight_details[4].text
-    status = flight_details[5].text
-    
-    print(f'Flight Number: {flight_num}')
-    print(f'From: {origin}')
-    print(f'To: {dest}')
-    print(f'Depart Time: {depart_time}')
-    print(f'Arrival Time: {arrival_time}')
-    print(f'Status: {status}')
-    print('-------------------------------------------')
+    print(flight.text)
